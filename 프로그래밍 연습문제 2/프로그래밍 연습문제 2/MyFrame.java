c907b91dbc5a77cea24c2ab1da4ad1c22fc8c76c
duveen <duveen@duveen.me>
2015-11-03 19:54:57 +0900

    프로그래밍 연습 2
    정말 대충했는데 올립니다.

---
+++ 프로그래밍 연습문제 2/프로그래밍 연습문제 2/MyFrame.java
@@ -0,0 +1,19 @@
+
+/**
+ * 여기에 MyFrame 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class MyFrame extends JFrame
+{
+    /**
+     * MyFrame 클래스의 객체 생성자
+     */
+    public MyFrame()
+    {
+        this.setSize(300.150);
+    }
+
+   
+}
